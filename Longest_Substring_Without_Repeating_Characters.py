1e5efb946d54632b48f214ef5f70b3a15a129255
Anuj Dutt <anuj.dutt9@gmail.com>
2020-05-06 11:07:02 -0400

    Create Longest_Substring_Without_Repeating_Characters.py

---
+++ Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,35 @@
+#  Longest Substring Without Repeating Characters
+# https://leetcode.com/explore/interview/card/apple/344/array-and-strings/2009/
+
+# Using Sliding Window Approach
+def lengthOfLongestSubstring(s: str) -> int:
+    # Starting Index
+    start = 0
+    # Length of Subsctring
+    max_len = 0
+    # Dictionary to hold characters
+    d = {}
+    
+    for i in range(len(s)):
+        if s[i] in d and start <= d[s[i]]:
+          # Increment Starting Index by 1
+          start = d[s[i]] + 1
+        else:
+          # Get Length of non repeatable characters
+          max_len = max(max_len, i - start + 1)
+        # Add character to Dictionary 
+        d[s[i]] = i
+    return max_len
+
+# Test Cases
+st = "abcabcbb"
+print(lengthOfLongestSubstring(st))
+assert lengthOfLongestSubstring(st) == 3
+
+st = "ABDEFGABEF"
+print(lengthOfLongestSubstring(st))
+assert lengthOfLongestSubstring(st) == 6
+
+st = "GEEKSFORGEEKS"
+print(lengthOfLongestSubstring(st))
+assert lengthOfLongestSubstring(st) == 7
