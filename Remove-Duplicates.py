e94749a3663c8eddd8280c71d3df701d1f526ca1
Anuj Dutt <anuj.dutt9@gmail.com>
2020-06-29 15:08:30 -0400

    Update Remove-Duplicates.py

--- Remove-Duplicates.py
+++ Remove-Duplicates.py
@@ -5,10 +5,16 @@ class Solution:
         if len(nums) == 0:
             return 0
         
+        # Keep track of Array Length
         l = 1
+        
+        # Since, we want to get the length in-place i.e.
+        # without using any extra memory
         for i in range(1, len(nums)):
+            # If the elements are not same
             if nums[i] != nums[i-1]:
+                # overwrite the element at index
                 nums[l] = nums[i]
+                # Increment the Length
                 l += 1
         return l
-               
