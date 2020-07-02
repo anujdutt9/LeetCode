b95a67c8be0d1b916d26340c0c46e9ba6c25f806
Anuj Dutt <anuj.dutt9@gmail.com>
2020-07-02 15:44:52 -0400

    Create Merge_Sorted_Arrays.py

---
+++ Merge_Sorted_Arrays.py
@@ -0,0 +1,8 @@
+# Merge Sorted Arrays
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2[:])
