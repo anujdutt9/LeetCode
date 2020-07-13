77f3084828fe0a94c6ee98c9d11aa93f5cde20a9
Anuj Dutt <anuj.dutt9@gmail.com>
2020-07-13 16:38:17 -0400

    Update Word_Break.py

--- Word_Break.py
+++ Word_Break.py
@@ -2,8 +2,17 @@
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        if s == '':
-            return True
-        else:
-            n = len(s)
-            return any([(s[:i] in wordDict) and self.wordBreak(s[i:], wordDict) for i in range(1, n + 1)])
+        if not s or not wordDict:
+            return False
+        
+        n = len(s)
+        ans = [False for u in range(n+1)]
+        ans[0] = True
+        
+        for i in range(n):
+            if ans[i]:
+                for word in wordDict:
+                    l = len(word)
+                    if i + l <= n and s[i: i+l] == word:
+                        ans[i + l] = True
+        return ans[-1]
