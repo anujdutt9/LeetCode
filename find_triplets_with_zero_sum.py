ad410f0bb52e0ae4c3f5146b0851b1754f9dd9dd
Anuj Dutt <anuj.dutt9@gmail.com>
2020-08-24 14:56:22 -0400

    Create find_triplets_with_zero_sum.py

---
+++ find_triplets_with_zero_sum.py
@@ -0,0 +1,44 @@
+# Triplets with Zero Sum
+
+def tripletZeroSum(nums):
+  if not nums:
+    return
+  
+  nums.sort()
+  total = 0
+  result = []
+
+  for i in range(len(nums) - 2):
+    if i > 0 and nums[i] == nums[i+1]:
+      continue
+    
+    left = i + 1
+    right = len(nums) - 1
+
+    while left < right:
+      total = nums[i] + nums[left] + nums[right]
+
+      if total > 0:
+        right -= 1
+      elif total < 0:
+        left += 1
+      else:
+        result.append([nums[i], nums[left], nums[right]])
+
+        while left < right and nums[left] == nums[left + 1]:
+          left += 1
+        
+        while left < right and nums[right] == nums[right - 1]:
+          right -= 1
+        
+        left += 1
+        right -= 1
+  
+  return False if len(result) == 0 else True
+
+# Test
+nums1 = [0, -1, 2, -3, 1]
+print(tripletZeroSum(nums1))
+
+nums2 = [1, 2, 3]
+print(tripletZeroSum(nums2))
